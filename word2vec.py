7305b19ec55740da9615905e15bf266a15ac482e
Hrithvik Gadhiya <47833530+hrithvik123@users.noreply.github.com>
2023-03-29 00:45:20 +0000

    add word2vec

---
+++ word2vec.py
@@ -0,0 +1,22 @@
+# Python program to generate word vectors using Word2Vec
+
+# importing all necessary modules
+import gensim
+from gensim.models import Word2Vec
+
+warnings.filterwarnings(action = 'ignore')
+
+# # Print results
+# print("Cosine similarity between 'alice' " +
+# 		"and 'wonderland' - Skip Gram : ",
+# 	model2.wv.similarity('alice', 'wonderland'))
+	
+# print("Cosine similarity between 'alice' " +
+# 			"and 'machines' - Skip Gram : ",
+# 	model2.wv.similarity('alice', 'machines'))
+
+def getWord2Vec(sentences):
+	print(sentences)
+	model = Word2Vec(sentences, min_count=1)
+	# print(model.wv.most_similar("A", topn=10))
+	return model
