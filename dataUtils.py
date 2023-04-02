be637f31dbec23e870840aa21c9ef0455fd2a2d4
hrithvik123 <gadhiyahrithvik@gmail.com>
2023-04-02 00:52:12 -0600

    update dataUtils

--- dataUtils.py
+++ dataUtils.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import bert
+# import bert
 import nlp
-import tfidf
-import word2vec
-import kmeans
+# import tfidf
+# import word2vec
+# import kmeans
 import recommendation_system
 
 # # For FilmTV dataset
@@ -34,10 +34,10 @@ print("\n")
 def combine_row(row):
     # corpus = str(row['title']) + ' ' + str(row['year']) + ' ' + str(row['genre']) + ' ' + str(row['country']) + ' ' + str(row['directors']) + ' ' + str(row['actors']) + ' ' + str(row['description'])
     corpus = str(row['genres']) + ' ' + str(row['description'])
-    lemmatized_corpus = nlp.lemmatizeSentence(corpus)
-    return lemmatized_corpus
+    # lemmatized_corpus = nlp.lemmatizeSentence(corpus)
+    return corpus
 
-dataset['combined_row'] = dataset.apply(combine_row, axis=1)
+dataset['description'] = dataset.apply(combine_row, axis=1)
 
 stringFor202StormCenter = "Alicia Hull (Davis) refuses to remove a pro-Communist book from the library where she works. We are in the deep province of America and it doesn't take long for the library to burn down in retaliation, but she holds out."
 
@@ -46,4 +46,4 @@ stringFor202StormCenter = "Alicia Hull (Davis) refuses to remove a pro-Communist
 # tfidf.extractQuery(dataset, stringFor202StormCenter)
 # w2v_embeddings = word2vec.getWord2Vec(dataset['combined_row'].tolist())
 
-recommendation_system.getRecommendations(dataset, stringFor202StormCenter)
+recommendation_system.getRecommendations(dataset, "A war veteran who is mentally unstable works as a taxi driver in New York City.")
